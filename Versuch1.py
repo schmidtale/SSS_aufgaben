31b22be3988bc0245d80ddd768d0067064c48a5c
schmidtale <schmidt.trossingen@web.de>
2024-11-01 10:32:09 +0100

    Protokoll hinzugefügt und Mittelwert usw. angepasst

--- Versuch1.py
+++ Versuch1.py
@@ -2,6 +2,9 @@ import os
 
 import numpy as np
 
+mean_list = None
+std_list = None
+
 
 # use genfromtxt to read the data from 11 files called V1_10.csv to V1_70.csv
 def read_data():
@@ -12,19 +15,30 @@ def read_data():
 
     converters = {i: lambda s: float(s.decode().replace(',', '.')) for i in range(2)}
 
-    data_list = []  # list to store the data from each file
+    global mean_list
+    global std_list
+
+    mean_list = []  # list to store the mean of the data from each file
+    std_list = []  # list to store the standard deviation of the data from each file
     for i in range(10, 71, 3):
         # Replace the German decimal comma with a dot and remove separators
-        csv_data = np.genfromtxt(f"{path}\\V1_{i}.csv", delimiter=";", skip_header=1000, skip_footer=1, dtype=float, converters=converters)
-        data_list.append(csv_data)
+        csv_data = np.genfromtxt(f"{path}\\V1_{i}.csv", delimiter=";", skip_header=1000, skip_footer=1, dtype=float,
+                                 converters=converters)
 
-    local_data = np.concatenate(data_list)
-    return local_data
+        # Write only voltage in array
+        voltage_data = csv_data[:, 1]
+
+        # get the mean of the data
+        mean = np.mean(voltage_data, axis=0)
+        print(f"{i} {mean}")
+        # get the standard deviation of the data
+        std = np.std(voltage_data, axis=0, ddof=1)
+        # print(f"{i} {std}")
+        mean_list.append(mean)
+        std_list.append(std)
+    return
 
 
 # call read_data() in main
 if __name__ == "__main__":
-    data = read_data()
-    print(data)
-    # print first 5 rows and columns
-    print(data.shape)
+    read_data()
