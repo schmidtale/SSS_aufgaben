742d4ee74ee4256c545f570f063dd31ab26d01bd
schmidtale <schmidt.trossingen@web.de>
2024-11-01 10:01:29 +0100

    Deutsches Komma

--- Versuch1.py
+++ Versuch1.py
@@ -10,14 +10,23 @@ def read_data():
     if not path:
         raise ValueError("DATA_PATH environment variable is not set")
 
-    local_data = np.array([])
+    converters = {i: lambda s: float(s.decode().replace(',', '.')) for i in range(2)}
+
+    data_list = []  # list to store the data from each file
     for i in range(10, 71, 3):
-        local_data = np.append(local_data, np.genfromtxt(f"{path}\\V1_{i}.csv", delimiter=";", skip_header=1000, skip_footer=1, dtype=float))
+        # Replace the German decimal comma with a dot and remove separators
+        csv_data = np.genfromtxt(f"{path}\\V1_{i}.csv", delimiter=";", skip_header=1000, skip_footer=1, dtype=str, converters=converters)
+        data_list.append(csv_data)
+
+    local_data = np.vstack(data_list)
     return local_data
 
 
 # call read_data() in main
 if __name__ == "__main__":
     data = read_data()
-    print(data)
+    #print(data)
+    # print first 5 rows and columns
+    print(data[:5, :5])
+
     print(data.shape)
